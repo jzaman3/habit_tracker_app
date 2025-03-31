43cf0ff0ebc2319a3d3f26bc1970d4e17dbf2ef4
jzaman3 <jzaman3@student.gsu.edu>
2025-03-31 16:10:29 -0400

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -19,8 +19,41 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: SplashScreen(), // Start with SplashScreen instead of HomePage
       theme: ThemeData(primarySwatch: Colors.green),
+    );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    // Wait for 3 seconds before navigating to the HomePage
+    Future.delayed(Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => HomePage()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lightGreen,
+      body: Center(
+        child: Icon(
+          Icons.emoji_food_beverage_outlined,
+          size: 100.0,
+          color: Colors.white,
+        ),
+      ),
     );
   }
 }
